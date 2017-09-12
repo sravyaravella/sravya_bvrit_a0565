1a624e3f67b594875e068b87a0baf393ab35d110
sravyaravella <31854088+sravyaravella@users.noreply.github.com>
2017-09-12 14:17:09 +0530

    Add files via upload

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -33,24 +33,24 @@ public class StudentGroup implements StudentArrayOperation {
 
 	@Override
 	public void setStudents(Student[] students) {
-		try {
+		/*try {
 			if(students==null)
 				throw new IllegalArgumentException("students unavailable");
 		}
 		catch(IllegalArgumentException e) {
 			System.out.println(e.getMessage());
-		}// Add your implementation here
+		}*/// Add your implementation here
 	}
 	@Override
 	public Student getStudent(int index) {
 		
-		try{
+		/*try{
 			if(index<0||index>students.length)
 				throw new IllegalArgumentException("Invalid Index");
 		}
 		catch(IllegalArgumentException e) {
 			System.out.println(e.getMessage());
-		}
+		}*/
 		// Add your implementation here
 		return null;
 	}
